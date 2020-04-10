3543dcacba5fb9fde103d66156773b5c58c6be61
mattericbrown <matt.eric.brown@gmail.com>
2020-04-10 11:59:42 -0700

    initial commit of frontend graphql Typescript Next.js app

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,44 @@
-import Link from 'next/link'
-import Layout from '../components/Layout'
+import { gql } from "apollo-boost";
+import Link from "next/link";
+import * as React from "react";
+import { Mutation } from "react-apollo";
+import Layout from "../components/Layout";
 
-const IndexPage = () => (
-  <Layout title="Home | Next.js + TypeScript Example">
-    <h1>Hello Next.js 👋</h1>
-    <p>
-      <Link href="/about">
-        <a>About</a>
-      </Link>
-    </p>
-  </Layout>
-)
+const IndexPage: React.FunctionComponent = () => {
+  return (
+    <Layout title="Home | Next.js + TypeScript Example">
+      <h1>hello Next.js 👋</h1>
+      <p>
+        <Link href="/about">
+          <a>About</a>
+        </Link>
+      </p>
+      <Mutation
+        mutation={gql`
+          mutation {
+            login(email: "boobs@boobs.com", password: "boobies") {
+              id
+              firstName
+              lastName
+              email
+              name
+            }
+          }
+        `}
+      >
+        {(mutate: () => any) => (
+          <button
+            onClick={async () => {
+              const response = await mutate();
+              console.log(response);
+            }}
+          >
+            call login mutation
+          </button>
+        )}
+      </Mutation>
+    </Layout>
+  );
+};
 
-export default IndexPage
+export default IndexPage;
