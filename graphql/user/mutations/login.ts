0486a06f4a3f0ec5c980079fe407c443beea2588
mattericbrown <matt.eric.brown@gmail.com>
2020-04-23 19:21:30 -0700

    update to user mutation Login

--- graphql/user/mutations/login.ts
+++ graphql/user/mutations/login.ts
@@ -1,11 +1,11 @@
 import { gql } from "apollo-boost";
 
 export const loginMutation = gql`
-         mutation Login($email: String!, $password: String!) {
-           login(email: $email, password: $password) {
-             id
-             userName
-             email
-           }
-         }
-       `;
+    mutation Login($email: String!, $password: String!) {
+      login(email: $email, password: $password) {
+        id
+        userName
+        email
+      }
+    }
+`;
