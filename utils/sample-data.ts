c8b1577ffd0bb591e22bb85b98c7106e6b53206a
mattericbrown <matt.eric.brown@gmail.com>
2020-04-23 20:46:51 -0700

    update sample data

--- utils/sample-data.ts
+++ utils/sample-data.ts
@@ -2,8 +2,8 @@ import { User } from '../interfaces'
 
 /** Dummy user data. */
 export const sampleUserData: User[] = [
-  { id: 101, name: 'Alice' },
-  { id: 102, name: 'Bob' },
-  { id: 103, name: 'Caroline' },
-  { id: 104, name: 'Dave' },
+  { id: 101, userName: 'Alice' },
+  { id: 102, userName: 'Bob' },
+  { id: 103, userName: 'Caroline' },
+  { id: 104, userName: 'Dave' },
 ]
